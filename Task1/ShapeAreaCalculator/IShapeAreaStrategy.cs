d137d8d06f886422cf30ff50bcb03237ea039f8f
Nikolay <Nikolay.Sviridov@norbit.ru>
2024-07-01 14:46:13 +0300

    Task 1 complete

---
+++ Task1/ShapeAreaCalculator/IShapeAreaStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace ShapeAreaCalculator
+{
+    public interface IShapeAreaStrategy
+    {
+        double Calculate(params double[] args);
+        string GetTypeOfShape(params double[] args);
+    }
+
+    public interface ICircleAreaStrategy : IShapeAreaStrategy
+    {
+        double Calculate(double radius);
+        string GetTypeOfShape();
+    }
+
+    public interface ITriangleAreaStrategy : IShapeAreaStrategy
+    {
+        double Calculate(double a, double b, double c);
+        string GetTypeOfShape(double a, double b, double c);
+    }
+}
